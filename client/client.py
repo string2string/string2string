75d8fa42f4f7637c2e49490749da91ccd64ee10c
Caleb Zulawski <zulaws@senior02.ee.cooper.edu>
2015-02-15 07:01:31 -0500

    fix server url

--- client/client.py
+++ client/client.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import string2string.string2string as s2s
 
-#client = s2s.Client('string2string.mybluemix.net')
-client = s2s.Client('hackcooper.cloudapp.net')
+client = s2s.Client('string2string.mybluemix.net')
+#client = s2s.Client('hackcooper.cloudapp.net')
 
 client.eraseAll()
 
